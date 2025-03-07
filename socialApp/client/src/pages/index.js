0fc6d42ad4a87f687ab999cc42a6fec70729853d
Prashant Raj <prashantraj9525@gmail.com>
2025-03-07 20:21:41 +0530

    all file clear in fullstack branch

--- socialApp/client/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "./Home/Home";
-import Login from "./login/Login";
-import Profile from "./profile/Profile";
-import Register from "./register/Register";
-
-
-export { Home,Profile,Login,Register };
